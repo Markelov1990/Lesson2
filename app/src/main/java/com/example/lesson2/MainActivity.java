c57ea487f607622dd1cf2ff5e2d7a190348f825f
Evgenii <markelovea1990@yandex.ru>
2022-11-20 01:39:01 +0300

    New class + full cycle

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lesson2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logEvent("onCreate");
+        if (savedInstanceState == null) {
+            logEvent("onCreate first");
+        }
+        else {
+            logEvent("onCreate relaunch");
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logEvent("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        logEvent("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logEvent("onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logEvent("onStop");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        logEvent("onSaveInstantState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logEvent("onDestroy");
+    }
+
+    private void logEvent(String event) {
+      Log.d ( "MainLog", event); }
+
 }
