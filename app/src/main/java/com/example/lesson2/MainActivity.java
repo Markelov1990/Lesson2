c7d785666517524bee70d46825a8ab02a69ab1d9
Evgenii <markelovea1990@yandex.ru>
2022-11-22 23:48:18 +0300

    All numbers are active and are in one method now

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.lesson2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,8 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-private int first_number = 0;
-private TextView result;
+    private int first_number = 0;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,85 +22,87 @@ private TextView result;
 
 
     }
-private void erstNummer() {
-    result = findViewById(R.id.first_number);
-    showResult();
 
-    findViewById(R.id.key_1).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 1;
-            showResult();
-        }
-    });
+    private void erstNummer() {
+        result = findViewById(R.id.first_number);
+        showResult();
 
-    findViewById(R.id.key_2).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 2;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_3).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 3;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_4).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 4;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_5).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 5;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_6).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 6;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_7).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 7;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_8).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 8;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_9).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 9;
-            showResult();
-        }
-    });
-    findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            first_number = 0;
-            showResult();
-        }
-    });
-}
-private void showResult () {
+        findViewById(R.id.key_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 1;
+                showResult();
+            }
+        });
+
+        findViewById(R.id.key_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 2;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 3;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 4;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 5;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 6;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 7;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 8;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 9;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 0;
+                showResult();
+            }
+        });
+    }
+
+    private void showResult() {
         result.setText(String.valueOf(first_number));
-}
+    }
 
 
     @Override
