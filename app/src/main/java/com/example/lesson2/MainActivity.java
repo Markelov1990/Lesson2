64ccd9c7c5c68e75fb718966653b350ef4320828
Evgenii <markelovea1990@yandex.ru>
2022-11-24 01:11:30 +0300

    Operation and numbers

--- app/src/main/java/com/example/lesson2/MainActivity.java
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -12,13 +12,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private int first_number = 0;
+    private int second_number = 0;
+    private final int count = 10;
+    private int operation = 0;
     private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        erstNummer();
+
+            erstNummer();
+            showResult();
+
 
 
     }
@@ -30,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.key_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 1;
+                first_number = first_number * count + 1;
                 showResult();
             }
         });
@@ -38,72 +45,223 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.key_2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 2;
+                first_number = first_number * count + 2;
                 showResult();
             }
         });
         findViewById(R.id.key_3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 3;
+                first_number = first_number * count + 3;
                 showResult();
             }
         });
         findViewById(R.id.key_4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 4;
+                first_number = first_number * count + 4;
                 showResult();
             }
         });
         findViewById(R.id.key_5).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 5;
+                first_number = first_number * count + 5;
                 showResult();
             }
         });
         findViewById(R.id.key_6).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 6;
+                first_number = first_number * count + 6;
                 showResult();
             }
         });
         findViewById(R.id.key_7).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 7;
+                first_number = first_number * count + 7;
                 showResult();
             }
         });
         findViewById(R.id.key_8).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 8;
+                first_number = first_number * count + 8;
                 showResult();
             }
         });
         findViewById(R.id.key_9).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                first_number = 9;
+                first_number = first_number * count + 9;
                 showResult();
             }
         });
         findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                first_number = first_number * count;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 first_number = 0;
+                operation = 0;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_plus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 1;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_minus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 2;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_multiply).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 3;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_divide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 4;
                 showResult();
             }
         });
     }
+    private void ZweiteNummer() {
+        result = findViewById(R.id.second_number);
+        showResult();
 
+        findViewById(R.id.key_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 1;
+                showResult();
+            }
+        });
+
+        findViewById(R.id.key_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 2;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 3;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 4;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 5;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 6;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 7;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 8;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count + 9;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                second_number = second_number * count;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                first_number = 0;
+                operation = 0;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_plus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 1;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_minus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 2;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_multiply).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 3;
+                showResult();
+            }
+        });
+        findViewById(R.id.key_divide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operation = 4;
+                showResult();
+            }
+        });
+    }
     private void showResult() {
         result.setText(String.valueOf(first_number));
+        if (operation == 1) { result.setText(String.valueOf(first_number + "  +")); }
+        if (operation == 2) { result.setText(String.valueOf(first_number + "  -")); }
+        if (operation == 3) { result.setText(String.valueOf(first_number + "  *")); }
+        if (operation == 4) { result.setText(String.valueOf(first_number + "  /")); }
+        if (operation == 4 && second_number !=0) { result.setText(String.valueOf(first_number + "  /  " + second_number)); }
     }
-
 
     @Override
     protected void onStart() {
